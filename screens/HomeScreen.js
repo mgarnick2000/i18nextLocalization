3684e45f265fe44a5ecb8afc1973cd8440ce2734
Mark <mgarnick@techtonicgroup.com>
2020-02-06 09:33:58 -0700

    install react-vector-icons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, {PureComponent} from 'react';
 import {View, Text, Button} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 class HomeScreen extends PureComponent {
   navToDetails = () => {
@@ -10,6 +11,7 @@ class HomeScreen extends PureComponent {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>Home Screen</Text>
+        <Icon name="home" size={25} color="blue" />
         <Button title="Navigate to Details" onPress={this.navToDetails} />
       </View>
     );
